efb9c98bc4c89e19c42ca004139b0e3fa604258f
Richard Morrisey <r.morrisey@computer.org>
2020-11-07 18:46:07 -0500

    all services working

--- RadMailTk.tcl
+++ RadMailTk.tcl
@@ -6,13 +6,19 @@ set sysInfo(log_dir) "$::env(HOME)/Dropbox/ham/Winlink/logs"
 set sysInfo(radio) "313"
 set sysInfo(cat_dev) "/dev/ttyCAT"
 set sysInfo(cat_baud) "19200"
-set sysInfo(cat_ptt) "/dev/ttyPTT"
+set sysInfo(ptt_dev) "/dev/ttyPTT"
 set sysInfo(ptt_ctrl) "DTR"
 set sysInfo(ardop_dir) "$::env(HOME)/Dropbox/ham/Winlink/ardop"
 set sysInfo(ardop_port) "8515"
 set sysInfo(winmor_port) "8500"
+set sysInfo(winmor_exe) "$::env(HOME)/prefix32/drive_c/RMS/Winmor\ TNC/Winmor\ TNC.exe"
+set sysInfo(proto) ardop
+set sysInfo(ardop-svcs) "rigctld ardopc ardop_gui pat browser"
+set sysInfo(winmor-svcs) "rigctld winmor ardopc ardop_gui pat browser"
 
-
+proc set_proto {proto} {
+  set sysInfo(proto) $proto
+}
 proc poll_proc {svc proc_name} {
   global svcInfo
 
@@ -26,19 +32,21 @@ proc poll_proc {svc proc_name} {
   update_indicator $svc
 }
 
-
 proc poll_rigctld {svc} {
   global svcInfo
 
-  set status [exec sudo systemctl --property=ActiveState --value show rigctld]
-  if {$status == "active"} {
-    set svcInfo(rigctld-running) 1
-  } elseif {$status == "inactive"} {
-    set svcInfo(rigctld-running) 0
-  }
-  if {[info exists svcInfo($svc-ind)]} {
-    update_indicator $svc
-  }
+#  --- This section is for daemon control ---
+#  set status [exec sudo systemctl --property=ActiveState --value show rigctld]
+#  if {$status == "active"} {
+#    set svcInfo(rigctld-running) 1
+#  } elseif {$status == "inactive"} {
+#    set svcInfo(rigctld-running) 0
+#  }
+#  if {[info exists svcInfo($svc-ind)]} {
+#    update_indicator $svc
+#  }
+
+  poll_proc $svc rigctld
 }
 
 proc poll_ardopc {svc} {
@@ -56,11 +64,23 @@ proc poll_pat {svc} {
 proc poll_browser {svc} {
   global svcInfo
 
-  set pids ""
   set lres [split [exec wmctrl -l] "\n"]
   puts $lres
   if {[lsearch -regexp $lres "Pat - Mailbox"] > 0} {
-    puts "found Pat - Mailbox"
+    set svcInfo($svc-running) 1
+  } else {
+    set svcInfo($svc-running) 0
+  }
+  update_indicator $svc
+
+}
+
+proc poll_winmor {svc} {
+  global svcInfo
+  set lres [split [exec wmctrl -l] "\n"]
+  puts "$lres"
+  puts [lsearch -regexp $lres "WINMOR"]
+  if {[lsearch -regexp $lres "WINMOR"] > 0} {
     set svcInfo($svc-running) 1
   } else {
     set svcInfo($svc-running) 0
@@ -71,32 +91,35 @@ proc poll_browser {svc} {
 
 proc toggle_rigctld {svc} {
   global svcInfo
+  global sysInfo
 
   if {$svcInfo($svc-running)} {
-    exec sudo systemctl stop rigctld
+    # exec sudo systemctl stop rigctld
+    exec pkill rigctld
   } else {
-    exec sudo systemctl start rigctld
+    # exec sudo systemctl start rigctld
+    exec rigctld -m $sysInfo(radio) \
+     -r $sysInfo(cat_dev) -s $sysInfo(cat_baud) \
+     -p $sysInfo(ptt_dev) -P $sysInfo(ptt_ctrl) &
   }
   after 100 "poll_rigctld $svc"
 
-#  exec rigctld -m$sysInfo(radio) -r$sysInfo(cat_dev) -s $sysInfo(cat_baud) \
-#    -p$sysInfo(ptt_dev) -p$sysInfo(ptt_ctrl)
 }
 
 proc toggle_winmor {svc} {
-  # Start Winmor TNC
-  # echo " --- Starting Winmor TNC ---"
-  # export WINEDEBUG=err,fixme-all
-  # export WINEPREFIX=$HOME/prefix32
-  # wine $WINEPREFIX/drive_c/RMS/Winmor\ TNC/Winmor\ TNC.exe &
-  # sleep 2
-  # ;;
+  global svcInfo
+  global sysInfo
 
-  # wmctrl -c WINMOR
-  # pid=$(ps -A |awk '/Winmor TNC.exe/ {print $1}')
-  # if [[ "$pid" -ne "" ]]; then
-  #  kill -9 $pid
-  # fi
+  if {$svcInfo($svc-running)} {
+    puts "Running  wmctrl -c WINMOR"
+    catch {exec -ignorestderr wmctrl -c WINMOR} res opts
+  } else {
+    puts "Running  wine $sysInfo(winmor_exe)"
+    set ::env(WINEDEBUG) "err,fixme-all"
+    set ::env(WINEPREFIX) "/home/rich/prefix32"
+    catch {exec -ignorestderr wine $sysInfo(winmor_exe) &} res opts
+  }
+  after 2500 "poll_winmor $svc"
 }
 
 proc toggle_browser {svc} {
@@ -144,7 +167,7 @@ proc toggle_pat {svc} {
   if {$svcInfo($svc-running)} {
     catch {exec pkill -x pat  2>/dev/null} res opts
   } else {
-    exec pat -l ardop http &
+    exec pat -l ardop,winmor http &
   }
   after 200 "poll_pat $svc"
 }
@@ -175,21 +198,25 @@ frame .mbar -borderwidth 1 -relief raised
 pack .mbar -fill x
 
 set menu [add_mbutton .mbar sys "System" left]
-
-$menu add command -label "rigctld" -command toggle
-$menu add command -label "ardopc" -command exit
-$menu add command -label "ardop_gui" -command exit
-$menu add command -label "WINMOR_TNC" -command exit
+$menu configure -tearoff 0
+set psel [menu $menu.psel]
+$psel configure -tearoff 0
+$psel add command -label "ardop" -command "set sysInfo(proto) ardop"
+$psel add command -label "winmor" -command "set sysInfo(proto) winmor"
+$menu add cascade -label "Protocol" -menu $psel
 
 set menu [add_mbutton .mbar ops "Operations" left]
+$menu configure -tearoff 0
 $menu add command -label "Launch Browser" -command launch_browser
 $menu add command -label "Connect" -command "pat_connect ardop:///kq4et?freq=3587.5"
 
 set menu [add_mbutton .mbar msg "Messages" left]
+$menu configure -tearoff 0
 $menu add command -label "Read" -command exit
 $menu add command -label "Compose" -command exit
 
 set menu [add_mbutton .mbar hlp "Help" right]
+$menu configure -tearoff 0
 $menu add command -label "Clear" -command exit
 
 proc do_toggle {svc} {
@@ -203,7 +230,6 @@ proc do_toggle {svc} {
   if {[winfo exists $svcInfo($svc-ind)]} {
      update_indicator $svc
   }
-
 }
 
 proc make_indicators {parent svcNames} {
@@ -214,7 +240,7 @@ proc make_indicators {parent svcNames} {
     set svcInfo($svc-button) $f.b
     set svcInfo($svc-ind) $f.i
     frame $f
-    pack $f -fill x -expand yes
+    pack $f -fill x -anchor w
 
     button $svcInfo($svc-button) -text $svc -command "$svcInfo($svc-cmd) $svc" \
       -font -*-lucida-medium-r-normal-sans-*-120-* -padx 1 -pady 1
@@ -235,21 +261,22 @@ proc update_indicator {svc} {
 }
 
 labelframe .ctrl_pnl
-pack .ctrl_pnl -side left -expand yes -fill y
+pack .ctrl_pnl -side left -fill y
 
-set svcs "rigctld ardopc ardop_gui pat browser"
-foreach svc "$svcs" {
+set sysInfo(proto) winmor
+
+foreach svc "$sysInfo($sysInfo(proto)-svcs)" {
   set svcInfo($svc-running) 0
   set svcInfo($svc-cmd) toggle_$svc
 }
 
-make_indicators .ctrl_pnl "$svcs"
-foreach svc "$svcs" {
+make_indicators .ctrl_pnl "$sysInfo($sysInfo(proto)-svcs)"
+foreach svc "$sysInfo($sysInfo(proto)-svcs)" {
   poll_$svc $svc
 }
 
 labelframe .f2 -text "RMS List"
-pack .f2 -side left -expand yes -fill y
+pack .f2 -side left -expand yes -fill y -anchor w
 
 frame .f2.f3 -width "3i" -relief sunken
 pack .f2.f3
